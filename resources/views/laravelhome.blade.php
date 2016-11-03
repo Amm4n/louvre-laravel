6a81c81793fb3114631ff367c177ac81f9aef973
Amman Sharif <amman.sharif07@gmail.com>
2016-11-03 15:46:17 +0800

    1st try? hopefully it workS!

---
+++ resources/views/laravelhome.blade.php
@@ -0,0 +1,74 @@
+<html>
+	<head>
+		<title>Laravel</title>
+		
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		<style>
+			body {
+				margin: 0;
+				padding: 0;
+				width: 100%;
+				height: 100%;
+				color: #B0BEC5;
+				display: table;
+				font-weight: 100;
+				font-family: 'Lato';
+			}
+
+			.container {
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+
+			.content {
+				text-align: center;
+				display: inline-block;
+			}
+
+			.title {
+				font-size: 96px;
+				margin-bottom: 40px;
+			}
+
+			.quote {
+				font-size: 24px;
+			}
+		</style>
+	</head>
+	<body>
+		{{-- <div class="container">
+			<div class="content">
+				<div class="title">Laravel 6</div>
+				<div class="quote">{{ Inspiring::quote() }}</div>
+			</div>
+		</div> --}}
+
+		{{-- # Include the Autoloader (see "Libraries" for install instructions)
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+
+# Instantiate the client.
+$mgClient = new Mailgun('key-be3fd258f9ab2f0ed92de347474664dd');
+$domain = "sandbox5682eabc93f9461090ac30baaa8df5dd.mailgun.org";
+
+# Make the call to the client.
+$result = $mgClient->sendMessage("$domain",
+                  array('from'    => 'Mailgun Sandbox <postmaster@sandbox5682eabc93f9461090ac30baaa8df5dd.mailgun.org>',
+                        'to'      => 'Amman Sarif <ammansarif@gmail.com>',
+                        'subject' => 'Hello Amman Sarif',
+                        'text'    => 'Congratulations Amman Sarif, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.')); --}}
+    
+
+		<div class="container">
+			<div class="content">
+				<div class="navbar">
+					<div class="navbar-inverse">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
